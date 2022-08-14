c5b8dd9d7fc08a9582bece54701fafeb01fd5b93
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-08-14 21:08:28 +0000

    Add files via upload

---
+++ apps/js/questions.js
@@ -0,0 +1,223 @@
+let questions = [
+    {
+        num: 1,
+        question: "What band was Harry Styles in before his solo career?",
+        answer: "One Direction",
+        options: [
+            "Westlife",
+            "One Direction",
+            "Little Mix",
+            "The Beach Boys"
+        ]
+    },
+    {
+        num: 2,
+        question: "What sport did David Beckham play?",
+        answer: "Soccer",
+        options: [
+            "Basketball",
+            "Rugby",
+            "Football",
+            "Soccer"
+        ]
+    },
+    {
+        num: 3,
+        question: "Gillian Anderson plays a therapist in which hit Netflix show?",
+        answer: "Sex Education",
+        options: [
+            "Tomorrow",
+            "The Sandman",
+            "Sex Education",
+            "First Kill"
+        ]
+    },
+    {
+        num: 4,
+        question: "What is the capital city of Australia?",
+        answer: "Canberra",
+        options: [
+            "Sydney",
+            "Lisbon",
+            "Madrid",
+            "Canberra"
+        ]
+    },
+    {
+        num: 5,
+        question: "How many planets are in our solar system?",
+        answer: "8",
+        options: [
+            "8",
+            "9",
+            "10",
+            "11"
+        ]
+    },
+    {
+        num: 6,
+        question: "What's the name of the talking snowman in Disney's Frozen?",
+        answer: "Olaf",
+        options: [
+            "Snowie",
+            "Scooby Doo",
+            "Olaf",
+            "Timon"
+        ]
+    },
+    {
+        num: 7,
+        question: "How many hearts does an octopus have?",
+        answer: "3",
+        options: [
+            "4",
+            "3",
+            "2",
+            "1"
+        ]
+    },
+
+    {
+        num: 8,
+        question: "In which year was the Microsoft XP operating system released?",
+        answer: "2001",
+        options: [
+            "2006",
+            "1998",
+            "2001",
+            "1992"
+        ]
+    },
+    {
+        num: 9,
+        question: "Who killed Tony Stark's parents?",
+        answer: "The Winter Soldier",
+        options: [
+            "The Winter Soldier",
+            "The Autumn Fireman",
+            "The Summer Police",
+            "Thanos"
+        ]
+    },
+    {
+        num: 10,
+        question: "Who discovered penicillin?",
+        answer: "Alexander Fleming",
+        options: [
+            "Isaac Newton",
+            "The Wright Brothers",
+            "Thomas Edison",
+            "Alexander Fleming"
+        ]
+    },
+    {
+        num: 11,
+        question: "Botany is the study of what life form?",
+        answer: "Plants",
+        options: [
+            "Plants",
+            "Insects",
+            "Fossils",
+            "Tissues"
+        ]
+    },
+    {
+        num: 12,
+        question: "Elon Musk is the CEO of which global brand.",
+        answer: "Tesla",
+        options: [
+            "Paypal",
+            "Microsoft",
+            "Tesla",
+            "Google"
+        ]
+    },
+    {
+        num: 13,
+        question: "With over 222 million units sold, what is Apple's highest-selling iPhone model?",
+        answer: "iPhone 6/6 Plus",
+        options: [
+            "iPhone 6/6 Plus",
+            "iPhone 5s",
+            "iPhone X",
+            "iPhone 7"
+        ]
+    },
+    {
+        num: 14,
+        question: "Where is the smallest bone in the human body located?",
+        answer: "Ear",
+        options: [
+            "Ear",
+            "Eye",
+            "Nose",
+            "Finger"
+        ]
+    },
+    {
+        num: 15,
+        question: "Glossectomy is the removal of all of or part of which body part?",
+        answer: "The Tongue",
+        options: [
+            "The Ear",
+            "The Mouth",
+            "The Hip",
+            "The Tongue"
+        ]
+    },
+    {
+        num: 16,
+        question: "Who plays John J Rambo in Rambo?",
+        answer: "Sylvester Stallone",
+        options: [
+            "Sylvester Stallone",
+            "Samuel L. Jackson",
+            "Chris Tucker",
+            "Jean Claude"
+        ]
+    },
+    {
+        num: 17,
+        question: "Which planet has the most moons?",
+        answer: "Saturn",
+        options: [
+            "Pluto",
+            "Venus",
+            "Mars",
+            "Saturn"
+        ]
+    },
+    {
+        num: 18,
+        question: "What is the largest animal on the planet?",
+        answer: "The Blue Whale",
+        options: [
+            "The Blue Whale",
+            "Elephant",
+            "Shark",
+            "Mouse"
+        ]
+    },
+    {
+        num: 19,
+        question: "Who invented the World Wide Web in 1990?",
+        answer: "Tim Berners-Lee",
+        options: [
+            "Tim Berners-Lee",
+            "Steve Jobs",
+            "Bill Gate",
+            "James Gosling"
+        ]
+    },
+    {
+        num: 20,
+        question: "Which planet is closest to the sun?",
+        answer: "Mercury",
+        options: [
+            "Earth",
+            "Mars",
+            "Mercury",
+            "Venus"
+        ]
+    },
+]
---
+++ apps/js/script.js
@@ -0,0 +1,187 @@
+const startBtn = document.querySelector(".start-btn button");
+const infoBox= document.querySelector(".info-box");
+const exitBtn = infoBox.querySelector(".btns .quit");
+const continueBtn = infoBox.querySelector(".btns .restart");
+const quizBox = document.querySelector(".quiz-section");
+const timeCounter = quizBox.querySelector(".timer .timer-sec");
+const timeLine = document.querySelector("header .time-line")
+const timeLineMobile = document.querySelector("header .time-line-mobile")
+
+const optionList = document.querySelector(".option-list");
+
+//Start Btn
+startBtn.addEventListener('click', function(){
+    infoBox.classList.add("activeInfo")
+})
+
+//Exit Btn
+exitBtn.addEventListener('click', function(){
+    infoBox.classList.remove("activeInfo")
+})
+
+//Continue Button When CLicked
+continueBtn.addEventListener('click', function(){
+    infoBox.classList.remove("activeInfo");
+    quizBox.classList.add("activeQuiz");
+    showQuestions(0);
+    questionCounter(1);
+    startTimer(15);
+    startTimerLine(0)
+    startTimerLineMobile(0)
+})
+
+
+let questionCount = 0;
+let bottomQuestionCounter = 1;
+let timerCount;
+let timeValue = 15;
+let widthValue = 0;
+let widthValueMobile = 0;
+
+
+const nextBtn = quizBox.querySelector('.next-btn');
+const resultBox = document.querySelector(".result-box");
+const restartQuiz = resultBox.querySelector(".btns .restart");
+const quitQuiz = resultBox.querySelector(".btns .quit");
+
+nextBtn.addEventListener('click', function(){
+    if (questionCount < questions.length - 1){
+        questionCount++;
+        bottomQuestionCounter++;
+        showQuestions(questionCount);
+        questionCounter(bottomQuestionCounter);
+        clearInterval(timerCount);
+        startTimer(timeValue);
+        clearInterval(counterLine);
+        startTimerLine(widthValue);
+        clearInterval(counterLineMobile);
+        startTimerLineMobile(widthValueMobile);
+        nextBtn.style.display = "none";
+    } else {
+        console.log("Completed")
+        showResultBox();
+    }
+})
+
+
+//Question and Options
+
+function showQuestions(index){
+    const questionText = document.querySelector(".question");
+    let questionTag = '<span>'+ questions[index].num + ". " + questions[index].question +'</span>'
+
+    let optionTag = '<div class="option flex flex-ai-c flex-jc-sb b-rad">'+ questions[index].options[0] + '<span></span></div>'
+                    + '<div class="option flex flex-ai-c flex-jc-sb b-rad">' + questions[index].options[1] + '<span></span></div>'
+                    + '<div class="option flex flex-ai-c flex-jc-sb b-rad">' + questions[index].options[2] + '<span></span></div>'
+                    + '<div class="option flex flex-ai-c flex-jc-sb b-rad">' + questions[index].options[3] + '<span></span></div>'
+    questionText.innerHTML = questionTag;
+    optionList.innerHTML = optionTag;
+    const option = optionList.querySelectorAll(".option");
+    for (let i = 0; i < option.length ; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+//When Option is selected
+let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>'
+let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>'
+
+function optionSelected(answer){
+    clearInterval(timerCount)
+    clearInterval(counterLine)
+    clearInterval(counterLineMobile)
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionCount].answer;
+    let allOptions = optionList.children.length;
+
+    if (userAnswer == correctAnswer){
+        answer.classList.add("correct");
+        console.log("Answer is Correct");
+        answer.insertAdjacentHTML("beforeend", tickIcon);
+    } else {
+        answer.classList.add("incorrect");
+        console.log("Answer is wrong");
+        answer.insertAdjacentHTML("beforeend", crossIcon);
+
+        //show correct answer after wrong answer is selected
+
+        for (let i = 0; i < allOptions; i++){
+            if(optionList.children[i].textContent == correctAnswer){
+                optionList.children[i].setAttribute("class", "option correct flex flex-ai-c flex-jc-sb");
+                optionList.children[i].insertAdjacentHTML("beforeend", tickIcon);
+            }
+        }
+    }
+
+    //disable options once one is selected
+
+    for (let i = 0; i < allOptions; i++) {
+        optionList.children[i].classList.add("disabled")
+        
+    }
+
+    nextBtn.style.display = "block";
+
+}
+
+
+
+function showResultBox(){
+    infoBox.classList.remove("activeInfo");
+    quizBox.classList.remove("activeQuiz");
+    resultBox.classList.add("activeResult");
+}
+
+
+
+function startTimer(time){
+    timerCount = setInterval(timer, 1000);
+    function timer(){
+        timeCounter.textContent = time;
+        time--;
+        if (time < 9){
+            let addZero = timeCounter.textContent;
+            timeCounter.textContent = "0" + addZero
+        }
+
+        if (time < 0){
+            clearInterval(timerCount)
+            timeCounter.textContent = "00";
+        }
+    }
+}
+
+
+function startTimerLine(time){
+    counterLine = setInterval(timer, 29);
+    function timer(){
+        time += 1;
+        timeLine.style.width = time + "px"
+        if (time > 549){
+            clearInterval(counterLine)
+        }
+    }
+}
+
+function startTimerLineMobile(time){
+    counterLineMobile = setInterval(timer, 32.5);
+    function timer(){
+        time += 1;
+        timeLineMobile.style.width = time + "px"
+        if (time > 385){
+            clearInterval(counterLineMobile)
+        }
+    }
+}
+
+
+
+
+
+
+//Bottom Counter
+function questionCounter(index){
+    const bottomCounter = quizBox.querySelector('.total');
+    let totalQuestionTag = '<span class="flex user-select"><p>'+ index + '</p>of<p>' + questions.length +'</p>Questions</span>';
+    bottomCounter.innerHTML = totalQuestionTag;
+}
